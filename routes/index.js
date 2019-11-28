4103ae05f2779b1596e73caa4cc367662b49eb69
risanto <potallegta@gmail.com>
2019-11-28 20:50:23 +0700

    minor fix

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ const router = require('express').Router()
 const user = require('./user')
 const pokemon = require('./pokemon')
 
-console.log('masuk index routes');
-
-
 router.use('/user', user)
 router.use('/pokemon', pokemon)
 
